a9c8d7d112b509800864e365f5fdbbca02d09e57
codeinner <codeinner>
2016-12-29 11:33:12 +0100

    Bash script exit status passthrough

--- make.sh
+++ make.sh
@@ -1,4 +1,6 @@
 mkdir build -p
 cd build
-cmake ./.. && cmake --build . --config Release && ctest . --output-on-failure
+cmake .. && cmake --build . --config Release && ctest . --output-on-failure
+status=$?
 cd ..
+exit $status
